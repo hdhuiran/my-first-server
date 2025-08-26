a3d2f149671148a118a22a21efc61786ee6b1f8a
hdhuiran <793079896@qq.com>
2025-08-26 17:12:49 +0800

    new post

--- index.js
+++ index.js
@@ -42,23 +42,78 @@
 // });
 //-----------------------------------------------------------------------------
 
-// 1. 引入所有需要的“工具包”
-const express = require("express");
-const { goods } = require("./data.js");
-const cors = require("cors"); // <-- 确保这一行存在
+// // 1. 引入所有需要的“工具包”
+// const express = require("express");
+// const { goods } = require("./data.js");
+// const cors = require("cors"); // <-- 确保这一行存在
+
+// const app = express();
+// const port = 3000;
+
+// // 2. 关键！把“通行证”贴在所有菜单规则的最前面
+// app.use(cors()); // <-- 确保这一行在所有 app.get/post 的上面
+
+// // 3. 我们的菜单规则
+// app.get("/goods", (req, res) => {
+//   console.log('当前后厨的菜单是:', goods);
+//   res.json(goods);
+// });
+
+// // 新增：“接订单”的规则 (POST)
+// app.post('/goods', (req, res) => {
+//   // req.body 就是我们从前端发送过来的新商品数据
+//   const newGood = req.body;
+  
+//   console.log('后厨收到了新的订单:', newGood);
+
+//   // 把新商品添加到我们的“菜单”里
+//   goods.push(newGood);
+
+//   // 回复顾客：“下单成功！”
+//   res.send('商品添加成功!');
+// });
+
+
+// // 4. 开门营业
+// app.listen(port, () => {
+//   console.log(`服务器已启动，正在监听 http://localhost:${port}`);
+// });
+//---------------------------------------------------------------------------------------------------
+
+const express = require('express');
+const { goods } = require('./data.js');
+const cors = require('cors');
 
 const app = express();
 const port = 3000;
 
-// 2. 关键！把“通行证”贴在所有菜单规则的最前面
-app.use(cors()); // <-- 确保这一行在所有 app.get/post 的上面
+app.use(cors());
 
-// 3. 我们的菜单规则
-app.get("/", (req, res) => {
+// 关键！安装“JSON解码器”，让我们的服务器能看懂 JSON 格式的请求体
+// 这一行必须放在所有路由规则的前面！
+app.use(express.json()); 
+
+// “查菜单”的规则 (GET)
+app.get('/goods', (req, res) => {
+  console.log('当前后厨的菜单是:', goods);
   res.json(goods);
 });
 
-// 4. 开门营业
+// “接订单”的规则 (POST)
+app.post('/goods', (req, res) => {
+  const newGood = req.body;
+  console.log('后厨收到了新的订单:', newGood);
+
+  // 我们在这里加一个保护，防止 undefined 被 push 进去
+  if (newGood && newGood.title && newGood.price) {
+    goods.push(newGood);
+    res.send('商品添加成功!');
+  } else {
+    // 如果收到的数据有问题，就回复一个错误
+    res.status(400).send('错误的商品数据');
+  }
+});
+
 app.listen(port, () => {
   console.log(`服务器已启动，正在监听 http://localhost:${port}`);
 });
